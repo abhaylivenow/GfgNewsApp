9e31a3c132ef4db37c8dc0fbf26225074cf5049d
abhaylivenow <56759264+abhaylivenow@users.noreply.github.com>
2022-01-29 18:54:57 +0530

    7. Converter factory added.

---
+++ app/src/main/java/com/example/gfgnewsapp/ui/NewsViewModelConverterFactory.kt
@@ -0,0 +1,14 @@
+package com.example.gfgnewsapp.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.gfgnewsapp.repository.NewsRepository
+
+class NewsViewModelProvider(
+    val newsRepository: NewsRepository,
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return NewsViewModel(newsRepository) as T
+    }
+}
